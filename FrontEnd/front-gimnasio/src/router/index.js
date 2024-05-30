d8b8420211401152c17db76b85ff192cf298448d
Jesus Flores Valencia <210336@utxicotepec.edu.mx>
2024-05-30 11:41:56 -0600

    Se agregaron rutas de las vistas creadas en menu-date y index

--- FrontEnd/front-gimnasio/src/router/index.js
+++ FrontEnd/front-gimnasio/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../components/Login.vue'
 import RegisterUserView from '../components/RegisterUser.vue'
+// import dashboardView from '@views/dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +16,18 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterUserView
-    }
+    },
+    
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/dynamic',
+      name: 'dynamic',
+      component: Dynamic
+    },
 
   ]
 })
