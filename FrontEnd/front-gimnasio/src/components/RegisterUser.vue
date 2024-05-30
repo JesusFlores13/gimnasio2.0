0e01077e85e009bb2b85a896499a2d7ab8ec3cb2
Jesus Flores Valencia <210336@utxicotepec.edu.mx>
2024-05-30 11:41:15 -0600

    modificacion de los archivos de registerUser bashboard y dinamicContent

--- FrontEnd/front-gimnasio/src/components/RegisterUser.vue
+++ FrontEnd/front-gimnasio/src/components/RegisterUser.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- component -->
     <div class="font-sans">
-        <div class="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 ">
+        <div class="relative min-h-screen flex flex-col  sm:justify-center items-center bg-gray-100 ">
             <div class="relative sm:max-w-sm w-full">
                 <div class="card bg-blue-400 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
                 <div class="card bg-red-400 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
@@ -11,63 +11,100 @@
                     </label>
                     <form method="#" action="#" class="mt-10">
 
-                        <div>
-                            <input type="text" placeholder="Nombres"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-
-                        <div class="mt-7">
-                            <input type="email" placeholder="Priemer Apellido"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-
-                        <div class="mt-7">
-                            <input type="password" placeholder="Segundo Apellido"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-
-                        <div class="mt-7">
-                            <input type="password" placeholder="Titulo de cortesia"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-                        <div class="mt-7">
-                            <input type="password" placeholder="Fecha de nacimiento"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-                        <div class="mt-7">
-                            <input type="password" placeholder="Foto"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-                        <div class="mt-7">
-                            <input type="password" placeholder="Seleccione el genero"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-                        <div class="mt-7">
-                            <input type="password" placeholder="Seleccione el tipo de sangre"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-
-                        <div class="flex mt-7 items-center text-center">
-                            <hr class="border-gray-300 border-1 w-full rounded-md">
-                            <label class="block font-medium text-base text-gray-600 ">
-                                Datos de acceso
-                            </label>
-                            <hr class="border-gray-300 border-1 w-full rounded-md">
-                        </div>
-
-                        <div class="mt-7">
-                            <input type="password" placeholder="Usuario"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-                        <div class="mt-7">
-                            <input type="password" placeholder="Password"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-                        <div class="mt-7">
-                            <input type="password" placeholder="Confirmara Password"
-                                class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0">
-                        </div>
-
+                        <div class="grid gap-12 mb-12 sm:grid-cols-2">
+                    <div>
+                        <label for="Nombre"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+                        <input type="text" name="nombre" id="nombre"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="nombre" required="">
+                    </div>
+                    <div>
+                        <label for="Primer Apellido"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Primer apellido</label>
+                        <input type="text" name="papellido" id="papellido"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="papellido" required="">
+                    </div>
+                    <div>
+                        <label for="Segundo Apellido"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Segundo apellido</label>
+                        <input type="text" name="sapellido" id="sapellido"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="sapellido">
+                    </div>
+                    <div>
+                        <label for="titulo"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titulo de cortesía</label>
+                        <input type="text" name="titulo" id="titulo"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Mr." required="">
+                    </div>
+                    <div>
+                        <label for="fechan"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de nacimiento</label>
+                        <input type="date" name="fechan" id="fechan"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    </div>
+                    <div>
+                        <label for="foto"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Foto</label>
+                        <input type="file" name="titulo" id="titulo"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Mr." required="">
+                    </div>
+                    <div>
+                        <label for="genero" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecciona el genero</label>
+                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option>Selecciona genero</option>
+                            <option>Hombre</option>
+                            <option>Mujer</option>
+                            <option>Otro</option>
+                      </select>
+                    </div>
+                    <div>
+                        <label for="sangre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Selecciona tipo de sangre</label>
+                        <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option>Selecciona tipo de sangre</option>
+                            <option>A+</option>
+                            <option>A-</option>
+                            <option>B+</option>
+                            <option>B-</option>
+                            <option>AB+</option>
+                            <option>AB-</option>
+                            <option>O+</option>
+                            <option>O-</option>
+                      </select>
+                    </div>
+                    
+                <h1 class="text-2xl xl:text-2xl font-extrabold">Datos de acceso</h1>
+                <br>
+                <div class="grid gap-12 mb-12 sm:grid-cols-2">
+                    <div>
+                        <label for="usuario"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Usuario</label>
+                        <input type="text" name="usuario" id="usuario"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Username" required="">
+                    </div>
+                    <div>
+                        <label for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" name="password" id="password"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="•••••••••" required="">
+                    </div>
+                    <div>
+                        <label for="confirm-password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                            password</label>
+                        <input type="password" name="confirm-password" id="confirm-password"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="•••••••••" required="">
+                    </div>
+                </div>  
+                    
+                </div>
                         <div class="mt-7">
                             <button
                                 class="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
