bded9c9984b6d2c68e9d31150a931c2e87dd83b4
Jesus FLores Valencia <valenciacerogpt@gmail.com>
2024-06-20 11:45:56 -0600

    configuracion del api para la insercion de usuarios

--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -1,8 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel 
+from datetime import datetime
 
 user = APIRouter()
+users =[]
+
+
+#userModel 
+class model_user(BaseModel):
+    id: str
+    usuario: str
+    contrasena: str
+    created_at:datetime = datetime.now()
+    estatus: bool=False
+
+
+
+@user.get("/")
+def bienvenida():
+    return "Bienvenido al sistema de apis"
+
 
 @user.get("/users")
 
-def helloword():
-    return "Hola 9B"
+def get_usuarios():
+    return users
+
+@user.post('/users')
+def save_usuarios(insert_users:model_user):
+    users.append(insert_users)
+    print(users)
+    return "Datos guardados"
