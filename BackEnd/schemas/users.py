4d0e0abe4f0c61f57e78506eb365e3ea0d1a87fb
Jesus Flores Valencia <valenciacerogpt@gmail.com>
2024-08-16 09:52:18 -0600

    correccin de errores tablas user y personas

--- BackEnd/schemas/users.py
+++ BackEnd/schemas/users.py
@@ -9,7 +9,7 @@ class UserBase(BaseModel):
     Nombre_Usuario:str
     Contrasena:str
     Correo_Electronico:str
-    Numero_Telefononico_Movil:str
+    Numero_Telefonico_Movil:str
     Estatus:str
     Fecha_Registro: datetime
     Fecha_Actualizacion: datetime
@@ -22,10 +22,11 @@ class UserUpdate(UserBase):
 
 class User(UserBase):
     Id:int
-    Persona_Id:int
+    #Persona_Id:int
     class Config:
         orm_mode = True
 
+       
 class UserLogin(BaseModel):
     Nombre_Usuario: Optional[str] = None
     Correo_Electronico: Optional[str]= None
